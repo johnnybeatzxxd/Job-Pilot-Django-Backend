eac7b82553a7f6dd1159cf42167b2247f7c6914c
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-12-19 12:19:01 +0300

    made the serializer dynamic

--- users/serializers.py
+++ users/serializers.py
@@ -10,15 +10,35 @@ class ProfileSerializer(serializers.ModelSerializer):
             'id',
             'user',
             'user_email',
+            'role',
             'full_name',
             'title',
             'country',
-            'hourly_rate',
             'email',
-            'skills',
             'bio',
             'profile_image',
             'created_at',
             'updated_at'
         ]
         read_only_fields = ['created_at', 'updated_at', 'user_email']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        
+        if instance.role == 'candidate':
+           
+            data['hourly_rate'] = instance.hourly_rate
+            data['skills'] = instance.skills
+            data['github'] = instance.github
+            data['linkedin'] = instance.linkedin
+        
+        elif instance.role == 'recruiter':
+            
+            data['company_name'] = instance.company_name
+            data['company_size'] = instance.company_size
+            data['industry'] = instance.industry
+            data['company_website'] = instance.company_website
+            data['linkedin'] = instance.linkedin
+            data['twitter'] = instance.twitter
+        
+        return data
